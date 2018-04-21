39b05a0b96ff4e1781ce587cef330a71706a7ba2
keita <yoshi.mori.pi.0710@gmail.com>
2018-04-21 22:04:04 +0900

    test

---
+++ prism_ani/src/Controller/TestsController.php
@@ -0,0 +1,19 @@
+
+<?php
+
+namespace App\Controller;
+
+class TestsController extends AppController
+{
+//    public $name = "Test";
+//    public $autoRender = false;
+
+//    public function index()
+//    {
+//        $this->name = 'test';
+//        $this->autoRender = false;
+        echo "aaa";
+        exit;
+//    }
+
+}
